592aa65ca735001717d46c789b8e7720475bd711
Dotan Reichman <dotanreichman@Dotans-MacBook-Pro.local>
2025-12-24 16:07:04 +0200

    done

--- IntToBin.java
+++ IntToBin.java
@@ -8,7 +8,17 @@ public class IntToBin {
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
-        //// Replace the following statement with your code
-        return "";
+        if(x<=0)
+            return "";
+        
+        String s;
+            if ((x % 2) == 0) {
+                s = toBinary(x/2)+"0";
+            }
+            else {
+                s = toBinary(x/2)+"1";
+            }
+              // integer division
+        return s;
     }    
  }
